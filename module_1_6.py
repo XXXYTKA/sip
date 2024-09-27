58978cffd38cbe24d61788cd145cb2d0cfee4d29
XXXYTKA <167037176+XXXYTKA@users.noreply.github.com>
2024-09-27 23:19:03 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'jake': 123452, 'bob': 82346, 'steve': 92384}
+print(my_dict)
+print(my_dict['bob'])
+my_dict['mike'] = 62135
+print(my_dict['mike'])
+my_dict['steve'] = 1209384, 923474723
+print(my_dict)
+my_set = [1, 2, 1, 4, 2, 3, 6, 2, 5, 3, 1, 2, 4]
+print(set(my_set))
+print(my_set.append(8))
+print(my_set.append(9))
+print(my_set)
+print(my_set.remove(4))
+print(my_set)
+
